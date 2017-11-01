ebce3421b4d945103aa85245843f8ad6f96989a3
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-10-31 23:14:41 -0300

    no me anda lpm

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,11 +1,16 @@
 "use strict";
+/// <reference path="../jquery/jquery.d.ts" />
+Object.defineProperty(exports, "__esModule", { value: true });
+var $ = require("jquery");
 var Startup = /** @class */ (function () {
     function Startup() {
     }
     Startup.main = function () {
         console.log('Hello World');
+        $(".btn-salir").first();
         return 0;
     };
     return Startup;
 }());
+exports.Startup = Startup;
 Startup.main();
--- public_html/js/index.ts
+++ public_html/js/index.ts
@@ -1,6 +1,11 @@
-class Startup {
+/// <reference path="../jquery/jquery.d.ts" />
+
+import * as $ from "jquery";
+
+export class Startup {
     public static main(): number {
         console.log('Hello World');
+        $(".btn-salir").first();
         return 0;
     }
 }
