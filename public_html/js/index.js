b88fe2f7ca72ac84a44a93a94ac79cec9ae9d5ce
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-02 23:26:19 -0300

    estoy re c bado

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -28,14 +28,27 @@ function crearCabecera(cols){
 function crearRows(cabecera, rows) {
     var newHtml = cabecera;
     newHtml += "<tbody>";
-    newHtml += "<tr>";
-    newHtml += "<th scope=\"row\"><input type=\"checkbox\" id=\"checkbox1\"></th>";
+    rows.forEach(function(row){
+        newHtml += "<tr>";
+        newHtml += "<th scope=\"row\"><input type=\"checkbox\" id=\"checkbox1\"></th>";
+        row.forEach(function(el){
+
+        });
+        newHtml = "</tr>";
+    });
 }
 
 
 window.onload = function() {
     var columnasAutos = ["Patente", "Usuario", "Fecha"];
-
+    var filas = [
+        ["columna1a", "columna2a", "columna3a"],
+        ["columna1b", "columna2b", "columna3b"],
+        ["columna1c", "columna2c", "columna3c"],
+        ["columna1d", "columna2d", "columna3d"],
+        ["columna1e", "columna2e", "columna3e"],
+        ["columna1f", "columna2f", "columna3f"]
+    ]
     crearGrilla(columnasAutos);
 };
 
