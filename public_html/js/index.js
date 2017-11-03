2ac2a9d8980513ecb75de95b214a28197f81d041
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-02 22:43:48 -0300

    vamo a flashiar cosas

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -8,6 +8,43 @@ function logout(){
     }
 }
 
-function agregarAuto() {
-    alert("lala");
+/*function agregarAuto() {
+   var currentHtml = $("#grilla").html();
+
+    currentHtml +=
+
+}*/
+
+function crearGrilla(cols){
+    console.log("pito");
+    var grilla = $("#grilla");
+    var newHtml = "";
+    grilla.html("");
+
+    newHtml += "<thead class=\"thead-dark\">";
+    newHtml += "<tr>";
+    newHtml += "<th scope=\"col\"></th>";
+
+    cols.forEach(function(col) {
+        newHtml += "<th scope=\"col\">" + col + "</th>";
+    });
+
+    newHtml += "</tr>";
+    newHtml = "</thead>";
 }
+
+crearGrilla(columnasAutos);
+
+
+var columnasAutos = ["Patente", "Usuario", "Fecha"];
+
+/*
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Patente</th>
+            <th scope="col">Usuario</th>
+            <th scope="col">Fecha</th>
+        </tr>
+    </thead>
+*/
--- public_html/js/login.js
+++ public_html/js/login.js
@@ -2,8 +2,22 @@ function login() {
     var email = $("#email-txt").val();
     var pass = $("#pass-txt").val();
 
-    if(true){ //validacion
+    if(false) { //No hay errores.
+        $("#input-group-mail").removeClass("has-error");
+        $("#input-group-pass").removeClass("has-error");
         window.location.href = "index.html";
-    }
+    } else 
+    {
+        if(true) { //Email tiene error
+            $("#input-group-mail").addClass("has-error");
+        } else {
+            $("#input-group-mail").removeClass("has-error");
+        }
+        if(true) { //Password tiene error
+            $("#input-group-pass").addClass("has-error");
+        } else {
+            $("#input-group-pass").removeClass("has-error");
+        }
+    } 
 
 }
