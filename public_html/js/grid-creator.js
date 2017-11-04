f5628b5fd9df275420a288821c5022b44654ae6c
JuanMaBF <juanmanuelbf@gmail.com>
2017-11-03 21:45:08 -0300

    fasdf

--- public_html/js/grid-creator.js
+++ public_html/js/grid-creator.js
@@ -1,46 +1,84 @@
 "use strict";
 ///<reference path="../node_modules/@types/jquery/index.d.ts"/>
-var grildCreator;
-(function (grildCreator) {
-    function crearGrilla(cols, rows) {
-        var cabecera = crearCabecera(cols);
-        let newHtml = crearRows(cabecera, rows);
-        $("#grilla").html(newHtml);
-    }
-    grildCreator.crearGrilla = crearGrilla;
-    function crearCabecera(cols) {
-        let newHtml = "";
-        newHtml += "<thead class=\"thead-dark\">";
-        newHtml += "<tr>";
-        newHtml += "<th scope=\"col\"></th>";
-        cols.forEach(function (col) { newHtml += "<th scope=\"col\">" + col + "</th>"; });
-        newHtml += "</tr>";
-        newHtml += "</thead>";
-        return newHtml;
-    }
-    function crearRows(cabecera, rows) {
-        let newHtml = cabecera;
-        newHtml += "<tbody>";
-        rows.forEach(function (row) {
+var gridCreator;
+(function (gridCreator_1) {
+    class gridCreator {
+        crearGrillaAutos() {
+            var columnas = ["Patente", "Usuario", "Fecha"];
+            var filas = [
+                ["columna1a", "columna2a", "columna3a"],
+                ["columna1b", "columna2b", "columna3b"],
+                ["columna1c", "columna2c", "columna3c"],
+                ["columna1d", "columna2d", "columna3d"],
+                ["columna1e", "columna2e", "columna3e"],
+                ["columna1f", "columna2f", "columna3f"]
+            ];
+            this.crearGrilla(columnas, filas);
+        }
+        crearGrillaUsuarios() {
+            var columnas = ["Nombre", "Estado"];
+            var filas = [
+                ["Pepito", "Funcionando"],
+                ["Pepito", "Eliminado"],
+                ["Pepito", "Sin aprobar"],
+                ["Pepito", "Bloqueado"]
+            ];
+            this.crearGrilla(columnas, filas);
+        }
+        crearGrillaPagos() {
+            var columnas = ["Patente", "Fecha", "Monto", "Usuario"];
+            var filas = [
+                ["MLD234", "12/06/17", "$500", "Pepito"],
+                ["MLD234", "12/06/17", "$500", "Pepito"],
+                ["MLD234", "12/06/17", "$500", "Pepito"],
+                ["MLD234", "12/06/17", "$500", "Pepito"]
+            ];
+            this.crearGrilla(columnas, filas);
+        }
+        crearGrilla(cols, rows) {
+            var cabecera = this.crearCabecera(cols);
+            let newHtml = this.crearRows(cabecera, rows);
+            $("#grilla").html(newHtml);
+        }
+        crearCabecera(cols) {
+            let newHtml = "";
+            newHtml += "<thead class=\"thead-dark\">";
             newHtml += "<tr>";
-            newHtml += "<th scope=\"row\"><input type=\"checkbox\" id=\"checkbox1\"></th>";
-            row.forEach(function (el) {
-                newHtml += "<td>" + el + "</td>";
-            });
+            newHtml += "<th scope=\"col\"></th>";
+            cols.forEach(function (col) { newHtml += "<th scope=\"col\">" + col + "</th>"; });
             newHtml += "</tr>";
-        });
-        return newHtml;
+            newHtml += "</thead>";
+            return newHtml;
+        }
+        crearRows(cabecera, rows) {
+            let newHtml = cabecera;
+            newHtml += "<tbody>";
+            rows.forEach(function (row) {
+                newHtml += "<tr>";
+                newHtml += "<th scope=\"row\"><input type=\"checkbox\" id=\"checkbox1\"></th>";
+                row.forEach(function (el) {
+                    newHtml += "<td>" + el + "</td>";
+                });
+                newHtml += "</tr>";
+            });
+            return newHtml;
+        }
     }
-})(grildCreator || (grildCreator = {}));
+    gridCreator_1.gridCreator = gridCreator;
+})(gridCreator || (gridCreator = {}));
 window.onload = function () {
-    var columnasAutos = ["Patente", "Usuario", "Fecha"];
-    var filas = [
-        ["columna1a", "columna2a", "columna3a"],
-        ["columna1b", "columna2b", "columna3b"],
-        ["columna1c", "columna2c", "columna3c"],
-        ["columna1d", "columna2d", "columna3d"],
-        ["columna1e", "columna2e", "columna3e"],
-        ["columna1f", "columna2f", "columna3f"]
-    ];
-    grildCreator.crearGrilla(columnasAutos, filas);
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaUsuarios();
 };
+function loadCarsGrid() {
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaAutos();
+}
+function loadUsersGrid() {
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaUsuarios();
+}
+function loadPaymentsGrid() {
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaPagos();
+}
--- public_html/js/grid-creator.ts
+++ public_html/js/grid-creator.ts
@@ -1,48 +1,94 @@
 ///<reference path="../node_modules/@types/jquery/index.d.ts"/>
 
-namespace grildCreator {
+namespace gridCreator {
 
-    export function crearGrilla(cols: Array<string>, rows: Array<Array<string>>) {
-        var cabecera = crearCabecera(cols);
-        let newHtml = crearRows(cabecera, rows);
-        $("#grilla").html(newHtml);
-    }
+    export class gridCreator {
 
-    function crearCabecera(cols: Array<string>){
-        let newHtml = "";
-        newHtml += "<thead class=\"thead-dark\">";
-        newHtml += "<tr>";
-        newHtml += "<th scope=\"col\"></th>";
-        cols.forEach(function(col: string) { newHtml += "<th scope=\"col\">" + col + "</th>"; });
-        newHtml += "</tr>";
-        newHtml += "</thead>";
-        return newHtml;
-    }
+        public crearGrillaAutos() {
+            var columnas = ["Patente", "Usuario", "Fecha"];
+            var filas = [
+                ["columna1a", "columna2a", "columna3a"],
+                ["columna1b", "columna2b", "columna3b"],
+                ["columna1c", "columna2c", "columna3c"],
+                ["columna1d", "columna2d", "columna3d"],
+                ["columna1e", "columna2e", "columna3e"],
+                ["columna1f", "columna2f", "columna3f"]
+            ];
+            this.crearGrilla(columnas, filas);
+        }
 
-    function crearRows(cabecera: string, rows: Array<Array<string>>) {
-        let newHtml = cabecera;
-        newHtml += "<tbody>";
-        rows.forEach(function(row){
+        public crearGrillaUsuarios() {
+            var columnas = ["Nombre", "Estado"];
+            var filas = [
+                ["Pepito", "Funcionando"],
+                ["Pepito", "Eliminado"],
+                ["Pepito", "Sin aprobar"],
+                ["Pepito", "Bloqueado"]
+            ];
+            this.crearGrilla(columnas, filas);
+        }
+
+        public crearGrillaPagos() {
+            var columnas = ["Patente", "Fecha", "Monto", "Usuario"];
+            var filas = [
+                ["MLD234", "12/06/17", "$500", "Pepito"],
+                ["MLD234", "12/06/17", "$500", "Pepito"],
+                ["MLD234", "12/06/17", "$500", "Pepito"],
+                ["MLD234", "12/06/17", "$500", "Pepito"]
+            ];
+            this.crearGrilla(columnas, filas);
+        }
+
+        private crearGrilla(cols: Array<string>, rows: Array<Array<string>>) {
+            var cabecera = this.crearCabecera(cols);
+            let newHtml = this.crearRows(cabecera, rows);
+            $("#grilla").html(newHtml);
+        }
+
+        private crearCabecera(cols: Array<string>){
+            let newHtml = "";
+            newHtml += "<thead class=\"thead-dark\">";
             newHtml += "<tr>";
-            newHtml += "<th scope=\"row\"><input type=\"checkbox\" id=\"checkbox1\"></th>";
-            row.forEach(function(el){
-                newHtml += "<td>" + el + "</td>";
-            });
+            newHtml += "<th scope=\"col\"></th>";
+            cols.forEach(function(col: string) { newHtml += "<th scope=\"col\">" + col + "</th>"; });
             newHtml += "</tr>";
-        });
-        return newHtml;
+            newHtml += "</thead>";
+            return newHtml;
+        }
+
+        private crearRows(cabecera: string, rows: Array<Array<string>>) {
+            let newHtml = cabecera;
+            newHtml += "<tbody>";
+            rows.forEach(function(row){
+                newHtml += "<tr>";
+                newHtml += "<th scope=\"row\"><input type=\"checkbox\" id=\"checkbox1\"></th>";
+                row.forEach(function(el){
+                    newHtml += "<td>" + el + "</td>";
+                });
+                newHtml += "</tr>";
+            });
+            return newHtml;
+        }
+
     }
 }
 
 window.onload = function() {
-    var columnasAutos = ["Patente", "Usuario", "Fecha"];
-    var filas = [
-        ["columna1a", "columna2a", "columna3a"],
-        ["columna1b", "columna2b", "columna3b"],
-        ["columna1c", "columna2c", "columna3c"],
-        ["columna1d", "columna2d", "columna3d"],
-        ["columna1e", "columna2e", "columna3e"],
-        ["columna1f", "columna2f", "columna3f"]
-    ]
-    grildCreator.crearGrilla(columnasAutos, filas);
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaUsuarios();
 };
+
+function loadCarsGrid() {
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaAutos();
+}
+
+function loadUsersGrid() {
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaUsuarios();
+}
+
+function loadPaymentsGrid() {
+    var grillaCreator = new gridCreator.gridCreator();
+    grillaCreator.crearGrillaPagos();
+}
