42580da9fb4848086e28802fc5b58b60bdae0527
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-05 22:52:25 -0300

    id

--- public_html/php/servidor.php
+++ public_html/php/servidor.php
@@ -11,14 +11,16 @@
             $grilla = new grillaObject;
             $grilla->cols = ["Patente", "Usuario", "Fecha"];
             $grilla->rows = [
-                ["columna1a", "columna2a", "columna3a"],
-                ["columna1b", "columna2b", "columna3b"],
-                ["columna1c", "columna2c", "columna3c"],
-                ["columna1d", "columna2d", "columna3d"],
-                ["columna1e", "columna2e", "columna3e"],
-                ["columna1f", "columna2f", "columna3f"]
+                ["1", "columna1a", "columna2a", "columna3a"],
+                ["1", "columna1b", "columna2b", "columna3b"],
+                ["1", "columna1c", "columna2c", "columna3c"],
+                ["1", "columna1d", "columna2d", "columna3d"],
+                ["1", "columna1e", "columna2e", "columna3e"],
+                ["1", "columna1f", "columna2f", "columna3f"]
             ];
             echo json_encode($grilla);
+            break;
+        case "otro":
             break;
     }
 
